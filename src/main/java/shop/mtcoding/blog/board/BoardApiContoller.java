7001e39c97e568d93f1615004ce8590e46beeea4
kjh5848~ <kjh5848@gmail.com>
2024-02-22 12:28:35 +0900

    delete ajax 통신 끝

--- src/main/java/shop/mtcoding/blog/board/BoardApiContoller.java
+++ src/main/java/shop/mtcoding/blog/board/BoardApiContoller.java
@@ -2,9 +2,12 @@ package shop.mtcoding.blog.board;
 
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.awt.desktop.PreferencesEvent;
 import java.util.List;
 
 @RestController
@@ -12,6 +15,17 @@ import java.util.List;
 public class BoardApiContoller {
 
     private final BoardRepository boardRepository;
+
+    @DeleteMapping("/api/boards/{id}")
+    public ApiUtil<?> deleteById(@PathVariable Integer id,HttpServletResponse response) {
+        Board board = boardRepository.selectOne(id);
+        if (board == null) {
+//            response.setStatus(404);
+            return new ApiUtil<>(404, "해당 게시글을 찾을 수 없습니다.", board);
+        }
+        boardRepository.deleteById(id);
+        return new ApiUtil<>(null);
+    }
 
     @GetMapping("/api/boards")
     public ApiUtil<?> findAll(HttpServletResponse response) {
--- src/main/java/shop/mtcoding/blog/board/BoardRepository.java
+++ src/main/java/shop/mtcoding/blog/board/BoardRepository.java
@@ -41,4 +41,14 @@ public class BoardRepository {
 
         query.executeUpdate();
     }
+
+    @Transactional
+    public void deleteById(Integer id) {
+        String a = """
+                delete from board_tb where id = ?;
+                """;
+        Query query = em.createNativeQuery(a);
+        query.setParameter(1, id);
+        query.executeUpdate();
+    }
 }
